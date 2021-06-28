90e720b76792e274e4ca3836e745103ecb5835c2
thiennt <thien.nguyen@big-labs.com>
2021-06-28 13:15:56 +0700

    update testing file

--- runCmd.py
+++ runCmd.py
@@ -6,15 +6,16 @@ import sys, getopt
 import os
 
 def default_arg_msg():
-    print('runCmd.py -t <type> -f <folder> -c <category>')
+    print('runCmd.py -t <type> -f <folder> -c <config> -C <backend_config>')
 
 if __name__ == '__main__':
     folder = ''
     parsetype = ''
-    category = ''
+    config = ''
+    backend_config = ''
     
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"t:f:c:",["type=","folder=", "category="])
+        opts, args = getopt.getopt(sys.argv[1:],"t:f:c:C:",["type=","folder=", "config=", "backend_config="])
     except getopt.GetoptError:
         default_arg_msg()
         sys.exit(2)
@@ -24,15 +25,18 @@ if __name__ == '__main__':
                 parsetype = arg
             elif opt in ("-f", "--folder"):
                 folder = arg
-            elif opt in ("-c", "--category"):
-                category = arg
+            elif opt in ("-c", "--config"):
+                config = arg
+            elif opt in ("-C", "--backend_config"):
+                backend_config = arg
     else:
         default_arg_msg()
         sys.exit()
     
     print('Load folder is "', folder)
     print('Parse type  is "', parsetype)  
-    print('Category  is "', category)
+    print('Config  is "', config)
+    print('Config Backend  is "', backend_config)
     
     onlyfiles = [os.path.join(r,file) for r,d,f in os.walk(folder) for file in f]
     
@@ -48,7 +52,7 @@ if __name__ == '__main__':
         if f in ignorelist:
             continue
         print("\n")
-        batcmd="python ./tools/sigmac -t " + parsetype + " " + f + " -c " + category
+        batcmd="python ./tools/sigmac -t " + parsetype + " " + f + " -c " + config + " -C " + backend_config
         print('======= ' + batcmd)
         result = subprocess.check_output(batcmd, shell=True, universal_newlines=False, encoding=False)
         print(result.decode("utf-8") , end = '', flush=True)
